7d8ad0ce0f86e5b12e678de94d65778a213cc0c1
rabunjauh <mustafa.visionet@gmail.com>
2020-11-08 17:31:10 +0700

    belajar javascript object

---
+++ javascript/object/script.js
@@ -0,0 +1,70 @@
+// membuat object
+
+// var mhs = {} object kosong
+
+// Object Literal
+// var mhs1 = {
+//     nama : "Mustafa",
+//     umur : 35,
+//     ips : [3.00, 2.50, 3.20],
+//     alamat : {
+//         jalan : "jl. abs No. 123",
+//         kota : "Batam",
+//         provinsi : "Kepri"
+//     }
+// };
+
+// var mhs2 = {
+//     nama : "Mahasiswa2",
+//     umur : 30,
+//     ips : [2.00, 2.50, 3.20],
+//     alamat : {
+//         jalan : "jl. xyz No. 321",
+//         kota : "Batam",
+//         provinsi : "Kepri"
+//     }
+// };
+
+// var obj = {};
+// obj.halo = function(){
+//     console.log('halo');
+// }
+// obj.halo();
+
+// 2. Function Declaration
+// function buatObjectMahasiswa(nama, nrp, email, jurusan){
+//     var mhs = {};
+//     mhs.nama = nama;
+//     mhs.nrp = nrp;
+//     mhs.email = email;
+//     mhs.jurusan = jurusan;
+//     return mhs; 
+// }
+
+// function halo(){
+//     console.log('halo');
+// }
+// halo();
+
+// var mhs3 = buatObjectMahasiswa('Mahasiswa3', '00908098', 'mahasiswa3@gmail.com', 'Teknik Perkapalan');
+// var mhs4 = buatObjectMahasiswa('Mahasiswa 4', '3143211', 'mahasiswa4@gmail.com', 'Akuntansi');
+
+// 3. Constructor
+// function Mahasiswa(nama, nrp, email, jurusan){
+//     this.nama = nama;
+//     this.nrp = nrp;
+//     this.email = email;
+//     this.jurusan = jurusan;
+// }
+
+// var mhs1 = new Mahasiswa('Mahasiswa 1', '45242534', 'mahasiswa@gmail.com', 'Teknik Informatika');
+
+// function Halo(){
+//     console.log('halo');
+// }
+
+// new Halo();
+
+// this
+// jika kita membuat this pada kontek global maka this merepresentasikan window
+// console.log(this); //this merepresentasikan window bisa juga ditulis console.log(window);
